9ded5a59bbcf90af706fd3b2acb634947349a8c9
Allzkie09 <157404837+Allzkie09@users.noreply.github.com>
2024-03-17 12:42:25 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const crypto = require('crypto');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
